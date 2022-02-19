f6e0d0d3506954208060ba3c9563ea5559414359
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-02-19 17:11:35 +0000

    switch where output goes

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -92,14 +92,14 @@ int initialise(const char* paramfile, const char* obstaclefile,
 ** timestep calls, in order, the functions:
 ** accelerate_flow(), propagate(), rebound() & collision()
 */
-int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
+int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles,t_speed* output);
 int accelerate_flow(const t_param params, t_speed* cells, int* obstacles);
 int propagate(const t_param params, t_speed* cells, t_speed* tmp_cells);
 int rebound(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
 int collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
 int write_values(const t_param params, t_speed* cells, int* obstacles, float* av_vels);
 
-float fushion(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles);
+float fushion(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles,t_speed* output);
 
 /* finalise, including freeing up allocated memory */
 int finalise(const t_param* params, t_speed** cells_ptr, t_speed** tmp_cells_ptr,
@@ -157,9 +157,12 @@ int main(int argc, char* argv[])
   init_toc = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
   comp_tic=init_toc;
 
+  t_speed* output     = NULL;
+  output = (t_speed*)malloc(sizeof(t_speed) * (params.ny * params.nx));
+
   for (int tt = 0; tt < params.maxIters; tt++)
   {
-    timestep(params, cells, tmp_cells, obstacles);
+    timestep(params, cells, tmp_cells, obstacles,output);
     //av_vels[tt] =fushion(params, cells, tmp_cells, obstacles);
     av_vels[tt] = av_velocity(params, cells, obstacles);
 #ifdef DEBUG
@@ -194,10 +197,10 @@ int main(int argc, char* argv[])
   return EXIT_SUCCESS;
 }
 
-int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles)
+int timestep(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles,t_speed* output)
 {
   accelerate_flow(params, cells, obstacles);
-  fushion(params, cells, tmp_cells, obstacles);
+  fushion(params, cells, tmp_cells, obstacles,output);
   //propagate(params, cells, tmp_cells);
   //rebound(params, cells, tmp_cells, obstacles);
   //collision(params, cells, tmp_cells, obstacles);
@@ -450,15 +453,14 @@ float av_velocity(const t_param params, t_speed* cells, int* obstacles)
   return tot_u / (float)tot_cells;
 }
 
-float fushion(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles){
+float fushion(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obstacles,t_speed* output){
   //CONSTS FROM COLLISION
   const float c_sq = 1.f / 3.f; /* square of speed of sound */
   const float w0 = 4.f / 9.f;  /* weighting factor */
   const float w1 = 1.f / 9.f;  /* weighting factor */
   const float w2 = 1.f / 36.f; /* weighting factor */
 
-  t_speed* output     = NULL;
-  output = (t_speed*)malloc(sizeof(t_speed) * (params.ny * params.nx));
+
 
 
   /* loop over _all_ cells */
@@ -593,9 +595,10 @@ float fushion(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
       }
     }
   }
-  
-  free(cells);
+
+  t_speed* temp = cells;
   *cells = *output;
+  *output = *temp;
 
   //temp = NULL;
 
