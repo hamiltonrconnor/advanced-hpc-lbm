c083e67b6460b08513db3e6988a70721c5c91fbb
Connor Hamilton <57008220+hamiltonrconnor@users.noreply.github.com>
2022-03-02 16:30:28 +0000

    Back to optimised serial

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -471,7 +471,7 @@ float fusion(const t_param params, t_speed** cells_ptr, t_speed** tmp_cells_ptr,
   //
     // for(int n=0; n<params.ny*params.nx; n++) {
     //   int ii = n/params.nx; int jj=n%params.nx;
-    #pragma omp parallel for collapse(2) reduction(+:tot_u,tot_cells)
+    //#pragma omp parallel for collapse(2) reduction(+:tot_u,tot_cells)
       for (int jj = 0; jj < params.ny; jj++)
       {
         for (int ii = 0; ii < params.nx; ii++)
